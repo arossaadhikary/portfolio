d6d159df6bb5c128a2d2c21a0cba4f345641cb47
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-15 15:35:21 -0400

    change: project updated

--- my-app/src/components/Projects.js
+++ my-app/src/components/Projects.js
@@ -3,6 +3,11 @@ import React from 'react'
 
 import sample from '../assets/preview.png';
 import githubIcon from '../assets/githubLogo.png';
+import outputIcon from '../assets/outputIcon.png';
+
+import WiNGHacksPreview from '../assets/Previews/WiNGHacksPreview.png';
+import AnimalCarePreview from '../assets/Previews/AnimalCarePreview.png';
+import PageTurnersPreview from '../assets/Previews/PageTurnersPreview.png';
 
 function Projects() {
   return (
@@ -12,50 +17,80 @@ function Projects() {
       {/* projects */}
       <div className="project_list">
 
-        <div className="project_item">
-          <img src={sample} className="project_img" alt="Project 1" />
+      <div className="project_item">
+          <img src={sample} className="project_img" alt="EWB Website" />
           <div className="project_details">
-            <h2>Project 1: Pantry Manager</h2>
-            <img src = {githubIcon} className = "navbar_icons"></img>
+            <h2>Engineering Without Borders Website</h2>
+            <a href = "https://github.com/ufewb/UF_EWB"><img src = {githubIcon} className = "navbar_icons"></img></a>
             <p>
               Pantry Manager is smart pantry operated by AI support chat. AI can suggest recipes based on ingredients stored, and store them.
             </p>
             <div className="project_skills">
-              <button>Skill 1</button>
-              <button>Skill 2</button>
-              <button>Skill 3</button>
+              <button>React Native</button>
             </div>
           </div>
         </div>
 
         <div className="project_item">
-          <img src={sample} className="project_img" alt="Project 2" />
+          <img src={PageTurnersPreview} className="project_img" alt="PageTurners" />
           <div className="project_details">
-            <h2>Project 2: Other</h2>
-            <img src = {githubIcon} className = "navbar_icons"></img>
+            <h2>PageTurners</h2>
+            <a href ="https://github.com/marin-isabella/PageTurners"><img src = {githubIcon} className = "navbar_icons"></img></a>
+            <a href = "https://www.youtube.com/watch?v=ARF130incFA&t=46s"><img src = {outputIcon} className = "navbar_icons"></img></a>
             <p>
               Pantry Manager is smart pantry operated by AI support chat. AI can suggest recipes based on ingredients stored, and store them.
             </p>
             <div className="project_skills">
-              <button>Skill 1</button>
-              <button>Skill 2</button>
-              <button>Skill 3</button>
+              <button>React Native</button>
+              <button>Expo Go</button>
+              <button>Flask</button>
             </div>
           </div>
         </div>
 
         <div className="project_item">
-          <img src={sample} className="project_img" alt="Project 3" />
+          <img src={WiNGHacksPreview} className="project_img" alt="WiNGHacks" />
           <div className="project_details">
-            <h2>Project 3: Other Again</h2>
-            <img src = {githubIcon} className = "navbar_icons"></img>
+            <h2>WiNGHacks</h2>
+            <a href="https://github.com/WiNGHacks/WiNGHacks-Website"><img src = {githubIcon} className = "navbar_icons"></img></a>
+            <a href="https://www.winghacks.com/"><img src = {outputIcon} className = "navbar_icons"></img></a>
             <p>
               Pantry Manager is smart pantry operated by AI support chat. AI can suggest recipes based on ingredients stored, and store them.
             </p>
             <div className="project_skills">
-              <button>Skill 1</button>
-              <button>Skill 2</button>
-              <button>Skill 3</button>
+              <button>React</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="project_item">
+          <img src={sample} className="project_img" alt="Earthquake Data Analysis" />
+          <div className="project_details">
+            <h2>Earthquake Data Analysis</h2>
+            <a href = "https://github.com/hugoliu-code/Group27-Final"><img src = {githubIcon} className = "navbar_icons"></img></a>
+            <p>
+              Pantry Manager is smart pantry operated by AI support chat. AI can suggest recipes based on ingredients stored, and store them.
+            </p>
+            <div className="project_skills">
+              <button>Python</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="project_item">
+          <img src={AnimalCarePreview} className="project_img" alt="Animal Care" />
+          <div className="project_details">
+            <h2>Animal Care</h2>
+            <a href ="https://github.com/hugoliu-code/DesignathonWinners"><img src = {githubIcon} className = "navbar_icons"></img></a>
+            <a href="https://www.youtube.com/watch?v=Vmt5MRfRNKI"><img src = {outputIcon} className = "navbar_icons"></img></a>
+            <p>
+              Leverges chatbots to educate pediatric patients about intimidating medical procedures.
+            </p>
+            <div className="project_skills">
+              <button>Unity</button>
+              <button>C#</button>
+              <button>Open AI</button>
+              <button>GPT 3.5</button>
             </div>
           </div>
         </div>
