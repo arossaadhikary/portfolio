aae1f397ce359bb45d200494b8ea8c0882f54625
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-15 16:21:02 -0400

    change: navigation bar, projects, about me

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -6,26 +6,17 @@ import headshot from '../assets/ArossaAdhikary_Headshot.jpg';
 function About() {
   return (
     <section className = "about_container" id="about">
-        <h2 className = "about_title>">About</h2>
+        <h1>ABOUT</h1>
         <div className = "about_content">
             <img src = {headshot} className = "about_img"></img>
             <ul className = "about_items">
                 <li className = "about_item">
                     <div>
-                        <h3>Frontend Developer</h3>
-                        <p>I'm a frontend dev. w/ expereince in building responsive & optimized websites</p>
-                    </div>
-                </li>
-                <li className = "about_item">
-                    <div>
-                        <h3>Backend Developer</h3>
-                        <p>I'm a frontend dev. w/ expereince in building responsive & optimized websites</p>
-                    </div>
-                </li>
-                <li className = "about_item">
-                    <div>
-                        <h3>UI Designer</h3>
-                        <p>I'm a frontend dev. w/ expereince in building responsive & optimized websites</p>
+                        <h3>Welcome to my developer journey. 👩‍💻</h3>
+                        <p>
+                            My name is Arossa - I'm an ambitious, goal-oriented, 2nd-year undergraduate honors student at the University of Florida. I strive to use my coding knowledge to make a social impact, with a specific interest in public service, protecting digital infrastructure, & education.
+                            I gravitate towards activities involving STEM education & mentorship, specifically for women & the youth. I'm aligning my career with artificial intelligence in the cybersecurity domain by studying for my Security+ certification & completing the Google Cybersecurity Certification Program with WiCYS. 
+                        </p>
                     </div>
                 </li>
             </ul>
--- my-app/src/components/NavigationBar.js
+++ my-app/src/components/NavigationBar.js
@@ -1,6 +1,5 @@
 import '../styles/NavigationBar.css';
 import React, { useState } from 'react'
-
 import closeIcon from '../assets/closeIcon.png';
 import menuIcon from '../assets/menuIcon.png';
 import linkedinIcon from '../assets/linkedinIcon.png';
@@ -9,39 +8,31 @@ import mailIcon from '../assets/mailLogo.png';
 
 function NavigationBar() {
     const [menuOpen, setMenuOpen] = useState(false)
-
     return (
         <nav className='navbar'>
-            <a href = "/" className = "navbar_title">@Arossa Adhikary</a>
-
+            <a href="/" className="navbar_title">@arossaadhikary</a>
             {/* navigation menu */}
-            <div className = "navbar_menu">
+            <div className="navbar_menu">
                 <img 
-                    className = "navbar_menuBtn" 
-                    src ={menuOpen ? closeIcon : menuIcon}
-                    onClick = {() => setMenuOpen(!menuOpen)} >
-                </img> 
-                <ul className = {'navbar_menuItems ${menuOpen && "navbar_menuOpen"}'} 
-                onClick = {() => setMenuOpen(false)}>
-                    <li>
-                        <a href ="#about">About Me</a> 
-                    </li>
-                    <li>
-                        <a href ="#projects">Projects</a> 
-                    </li>
-                    <li>
-                        <a href ="#expereince">Experience</a> 
-                    </li>
-                    <li>
-                        <a href ="#contact">Contact</a> 
-                    </li>
+                    className="navbar_menuBtn" 
+                    src={menuOpen ? closeIcon : menuIcon}
+                    onClick={() => setMenuOpen(!menuOpen)}
+                    alt="Menu"
+                />
+                <ul className={`navbar_menuItems ${menuOpen ? "navbar_menuOpen" : ""}`}
+                    onClick={() => setMenuOpen(false)}>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#experience">Experience</a></li>
+                    <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>  
-
             {/* icons */}
-            <img src = {linkedinIcon} className = "navbar_icons"></img>
-            <img src = {githubIcon} className = "navbar_icons"></img>
-            <img src = {mailIcon} className = "navbar_icons"></img>
+            <div className="navbar_icon_container">
+                <img src={linkedinIcon} className="navbar_icons" alt="LinkedIn" />
+                <img src={githubIcon} className="navbar_icons" alt="GitHub" />
+                <img src={mailIcon} className="navbar_icons" alt="Email" />
+            </div>
         </nav>
     )
 }
