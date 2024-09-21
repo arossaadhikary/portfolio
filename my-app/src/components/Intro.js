66ae567ef7926fac64086107f84556debaf1823f
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2024-09-21 00:23:21 -0400

    test

--- my-app/src/components/Intro.js
+++ my-app/src/components/Intro.js
@@ -7,9 +7,9 @@ import headshot from '../assets/ArossaAdhikary_Headshot_Website.png';
 function Intro() {
   return (
     <section className = "intro_container">
-        <div className = "hero_content">
+        <div className = "intro_content">
             <h1 className = "intro_title">Hello - I'm Arossa 👋</h1>
-            <p className = "hero_description">
+            <p className = "intro_description">
                 I'm a passionate developer from Tampa, FL. 
                 I enjoy creating web/IOS applications & am looking to expand into the cybersecurity sphere. 
                 I have experience with Javascript/React, C++,  Python.
