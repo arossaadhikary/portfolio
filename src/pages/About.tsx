556eb563cdc80c47601c4287661b08cee1279cc2
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2026-09-17 16:25:15 -0400

    update w shifting motion img to abt section

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,5 @@
-import AboutHeadshot from "../assets/aboutHeadshot.png"
+import AboutHeadshot from "../assets/Headshot.png"
+import { motion } from "framer-motion"
 
 const About = () => {
     return (
@@ -7,13 +8,27 @@ const About = () => {
             <hr className="mb-6 border-slate-700"></hr>
             {/* Grid Wrapper */}
             <div className="grid grid-cols-1 md:grid-cols-2 items-center">
-                <img src={AboutHeadshot} className="mx-auto max-w-[220px] sm:max-w-sm md:max-w-md rounded-full"></img>
+                {/* Image */}
+                <motion.div 
+                    className="order-1 md:order-2 flex justify-center"
+                    initial={{ opacity: 0, scale: 0.9 }}
+                    animate={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 1, delay: 0.2, ease: "easeOut" }}
+                >
+                <img
+                    src={AboutHeadshot}
+                    alt="Headshot"
+                    className="mx-auto max-w-[200px] sm:max-w-[300px] md:max-w-md rounded-full animate-float"
+                />
+                </motion.div>                
+
+
                 <div className="text-left sm:text-lg md:text-lg text-slate-300 leading-relaxed max-w-lg mx-auto md:mx-0">
                     <p className="mb-5 md:mb-10">
                         My name is Arossa - I'm a 4th-year Computer Science student at the University of Florida (Go Gators!). 
                     </p>
                     <p>
-                        I build with people in mind. I’m interested in the intersection of product, software, AI, cloud infrastructure, and cybersecurity. I enjoy taking ideas from ambiguity to something people can actually use. I bring this mindset by shaping the product, building the experience, and understanding the infrastructure behind it. 
+                        I’m interested in the intersection of product, software, AI, cloud infrastructure, and cybersecurity. I enjoy taking ideas from ambiguity to something people can actually use. I bring this mindset by shaping the product, building the experience, and understanding the infrastructure behind it. 
                         <br></br> <br></br> Outside of tech, I’m passionate about STEM education and mentorship through organizations such as Women in Computer Science & Engineering, Kode With Klossy, and Girls Who Code.            
                     </p>
                 </div>
