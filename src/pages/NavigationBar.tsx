c11b5c924f21b1f07492a041c6c7c7920548ad7a
arossaadhikary <148636324+arossaadhikary@users.noreply.github.com>
2026-09-18 00:46:36 -0400

    update preview.png

--- src/pages/NavigationBar.tsx
+++ src/pages/NavigationBar.tsx
@@ -11,6 +11,8 @@ const NavigationBar = () => {
   // link = react router navigation (differ page nav through sep route)
   // href normal browser navigation (use for scroll same page)
 
+  // to-do: have items highlighted on page 
+  
   return (
     <nav className="absolute top-5 left-0 z-50 w-full flex justify-between items-center px-6 sm:px-8 py-4 text-white">
       {/* Logo/Handle */}
